6082abeaf4b9b1be84b180c8a74e14fad54a5669
Rithesh <asanthularithesh@gmail.com>
2025-04-27 23:10:42 +0530

    created the schema for the scores

--- ATS/app/routes/gemini_routes.py
+++ ATS/app/routes/gemini_routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, request, jsonify
 from app.middleware.auth_middleware import verify_jwt
-from app.controllers.gemini_controllers import start_interview, answer_question,summary_of_text,calculate_score
+from app.controllers.gemini_controllers import start_interview, answer_question,summary_of_text,calculate_score,get_calculated_score
 
 gemini_bp = Blueprint("gemini", __name__)
 
@@ -22,3 +22,7 @@ def summarizer():
 @verify_jwt
 def calculate(id):
     return calculate_score(id)
+@gemini_bp.route("get-calculated-score/<id>",methods=["GET"])
+@verify_jwt
+def getCalculatedScore(id):
+    return get_calculated_score(id)
