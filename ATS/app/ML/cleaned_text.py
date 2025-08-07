8fc2381ca72a4f5a9f2a983c914ab0897bee371a
Rithesh <asanthularithesh@gmail.com>
2025-08-07 17:29:41 +0530

    edited

--- ATS/app/ML/cleaned_text.py
+++ ATS/app/ML/cleaned_text.py
@@ -5,21 +5,28 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
-# nltk.download('punkt_tab')
-# Initialize Lemmatizer
+# Download only if not already present
+try:
+    stop_words = set(stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+
 lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words('english'))
 
 def clean_text(text):
     text = text.lower()
-
-    text = re.sub(r'[^a-z\s]', '', text)  
-
+    text = re.sub(r'[^a-z\s]', '', text)
     tokens = word_tokenize(text)
-
     processed_tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
-
     return " ".join(processed_tokens)
